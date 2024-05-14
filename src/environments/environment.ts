0160d4feb5eddac261d9fd7d142eab9ab1ed2ce7
acoria1 <60652967+acoria1@users.noreply.github.com>
2024-05-14 00:06:13 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyCdRRnBbX8R0qg3xauDHItMVmvdDAdzlxk",
+    apiKey: "",
     authDomain: "ionic-login-a9669.firebaseapp.com",
     projectId: "ionic-login-a9669",
     storageBucket: "ionic-login-a9669.appspot.com",
     messagingSenderId: "698057746644",
-    appId: "1:698057746644:web:7558813a2e58ae92c73341",
+    appId: "1::web:",
     locationId: 'us-central',
   },
   production: false
